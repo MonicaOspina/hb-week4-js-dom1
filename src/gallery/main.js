4c15cab28c922ffff9cc0036ec223a031fdbda50
MonicaOspina <monipoc@gmail.com>
2017-12-15 08:28:52 -0500

    dding arrow

--- src/gallery/main.js
+++ src/gallery/main.js
@@ -1,13 +1,58 @@
+var pos = 0;
+
 var image = document.querySelectorAll('.gallery__images-item') // toma todos los elementos
+var rightbtn = document.querySelector('.gallery__control-right')
+rightbtn.addEventListener("click",next)
+var leftbtn = document.querySelector('.gallery__control-left')
+leftbtn.addEventListener("click",back)
+var dots = document.querySelectorAll('.gallery__dots-button') 
+//dots.addEventListener("click",next)
 
-console.log('cambio')
-console.log(image)
-console.log(image[0])
-console.log(image[5])
 
-image[0].classList.remove('gallery__images-item--selected')
-image[7].classList.add('gallery__images-item--selected')
+function next(){
+    if (pos >=0 && pos < image.length-1) {
+            if (pos === 0){
+                leftbtn.classList.add('gallery__control--disabled')
+            }
+            if (pos === image.length-1){
+                rightbtn.classList.add('gallery__control--disabled')
+            }
+        image[pos].classList.remove('gallery__images-item--selected')
+        pos = pos+1
+        leftbtn.classList.remove('gallery__control--disabled')
+        image[pos].classList.add('gallery__images-item--selected')
+        console.log(image[pos])
+    }
+}
 
-console.log(image[0])
-console.log(image[5])
-// console.log(image)
+function back(){
+    if (pos > 0 && pos <= image.length-1) {
+        /*function disabledbtn(){
+            if (pos == 0){
+                leftbtn.classList.add('gallery__control-left--disabled')
+            }
+            else{
+                leftbtn.classList.remove('gallery__control-left--disabled')
+            }
+            if (pos == image.length-1){
+                rightbtn.classList.add('gallery__control-right--disabled')
+            }
+            else{
+                rigthbtn.classList.remove('gallery__control-rigth--disabled')
+            }
+            console.log(pos)
+        }*/
+        image[pos].classList.remove('gallery__images-item--selected')
+        pos = pos-1
+        image[pos].classList.add('gallery__images-item--selected')
+        console.log(image[pos])
+    }
+}
+/*function disabledbtn(){
+    if (pos === 0){
+        leftbtn.classList.add('gallery__control-left--disabled')
+    }
+    if (pos === image.length-1){
+        rigthbtn.classList.add('gallery__control-rigth--disabled')
+    }
+}*/
